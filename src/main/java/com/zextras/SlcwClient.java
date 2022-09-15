2f888ff3eac21e149c36301c9e36f52b6c564e53
Davide Polonio <davide.polonio@zextras.com>
2022-09-15 17:03:12 +0200

    chore(SM-127): add project scaffold

---
+++ src/main/java/com/zextras/SlcwClient.java
@@ -0,0 +1,9 @@
+package com.zextras;
+
+/** FIXME add actual documentation when it will be implemented */
+public class SlcwClient {
+  // FIXME remove this when actually implementing the codebase :)
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
