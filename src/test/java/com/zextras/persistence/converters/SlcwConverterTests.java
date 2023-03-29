19e22aad83e737816dbb7f7846507d84cd839ce8
Davide Frison <davide.frison@zextras.com>
2023-03-29 12:24:33 +0200

    chore: cleanup removed classes usage

--- src/test/java/com/zextras/persistence/converters/SlcwConverterTests.java
+++ src/test/java/com/zextras/persistence/converters/SlcwConverterTests.java
@@ -1,18 +1,8 @@
 package com.zextras.persistence.converters;
 
-import com.zextras.persistence.mapping.SlcwProperty;
 import java.util.HashMap;
-import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 
 class SlcwConverterTests {
 
-
-  @BeforeEach
-  void setUp() {
-    Map<String, SlcwProperty> fields = new HashMap<>();
-    fields.put("givenName", new SlcwProperty("name", "Name"));
-    fields.put("sn", new SlcwProperty("surname", "Surname"));
-    fields.put("homePhone", new SlcwProperty("phoneNumber", 675479980));
-  }
 }
