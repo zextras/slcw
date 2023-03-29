fa60278b74df2f01cbc8792301170d28df876a2a
Davide Frison <davide.frison@zextras.com>
2023-03-29 12:20:52 +0200

    refactor: remove transcoders, entities and mapping

--- src/main/java/com/zextras/persistence/mapping/entries/Entry.java
+++
@@ -1,8 +0,0 @@
-package com.zextras.persistence.mapping.entries;
-
-/**
- * A parent class for all the entry classes which represent records in the given structure.
- */
-public abstract class Entry {
-
-}
